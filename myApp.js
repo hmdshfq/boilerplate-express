583b373dc1b430b990784aedc7d634b923adf3d3
Hammad <hmdshfq@outlook.com>
2023-08-15 00:29:04 +0200

    add middleware

--- myApp.js
+++ myApp.js
@@ -2,7 +2,14 @@ let express = require('express');
 let app = express();
 
 let entryPath = `${__dirname}/views/index.html`;
+let staticPath = `${__dirname}/public`;
 
+/* 
+    Adding middleware - A middleware is a function that intercepts
+    the route handlers and adds some kind of information. It needs to be mounted using the app.use(path, middleware function) method.
+*/
+
+app.use(staticPath, express.static())
 app.get('/', (request, response) => response.sendFile(entryPath));
 
 
