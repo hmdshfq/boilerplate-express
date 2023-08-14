d38299d1db9b36aaa353f69418a1d85c009ad957
Hammad <hmdshfq@outlook.com>
2023-08-15 00:30:19 +0200

    add middleware comment

--- myApp.js
+++ myApp.js
@@ -7,6 +7,8 @@ let staticPath = `${__dirname}/public`;
 /* 
     Adding middleware - A middleware is a function that intercepts
     the route handlers and adds some kind of information. It needs to be mounted using the app.use(path, middleware function) method.
+
+    The express.static() middleware serves the files in the static (public) folder.
 */
 
 app.use(staticPath, express.static())
