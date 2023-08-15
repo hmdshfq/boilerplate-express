cce097b1c4eeef03244c9e419c8caf377125730a
Hammad <hmdshfq@outlook.com>
2023-08-15 14:38:45 +0200

    add middleware

--- myApp.js
+++ myApp.js
@@ -2,18 +2,18 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
-let entryPath = `${__dirname}/views/index.html`;
-let staticPath = `${__dirname}/public`;
-
 /* 
     Adding middleware - A middleware is a function that intercepts
-    the route handlers and adds some kind of information. It needs to be mounted using the app.use(path, middleware function) method.
+    the route handlers and adds some kind of information. It needs to be mounted
+    using the app.use(path, middleware function) method.
 
     The express.static() middleware serves the files in the static (public) folder.
 */
 
+let staticPath = `${__dirname}/public`;
 app.use('/public', express.static(staticPath));
 
+let entryPath = `${__dirname}/views/index.html`;
 app.get('/', (request, response) => response.sendFile(entryPath));
 
 let responseObject = {
@@ -27,5 +27,10 @@ app.get('/json', (request, response) => {
         return response.json(responseObject);
     }
 });
+
+app.use('/', (request, response, next) => {
+    console.log(`${request.method} ${request.path} - ${request.ip}`);
+    next();
+})
 
 module.exports = app;
