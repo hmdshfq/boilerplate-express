5e6d97c9a59d2a2f7c542e38459dcc5a0c2f11e6
Hammad <hmdshfq@outlook.com>
2023-08-15 16:54:28 +0200

    add comments

--- myApp.js
+++ myApp.js
@@ -2,9 +2,6 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 require('dotenv').config();
-let urlEncodedMiddleware = bodyParser.urlencoded({
-    extended: false
-})
 
 /* 
     Adding middleware - A middleware is a function that intercepts
@@ -19,39 +16,64 @@ let urlEncodedMiddleware = bodyParser.urlencoded({
 let staticPath = `${__dirname}/public`;
 app.use('/public', express.static(staticPath));
 
+/* 
+    Mount a middleware that logs to the console method path - IP
+    For example:
+    GET / - ::1
+*/ 
 app.use((request, response, next) => {
     console.log(`${request.method} ${request.path} - ${request.ip}`);
     next();
 });
 
-app.get('/now', (req, res, next) => { 
-    req.time = new Date().toString();
-    next();
-}, (req, res, next) => {
-    res.send({
-        time: req.time
-    });
-})
+app.get(
+    '/now',
+    (req, res, next) => {
+        req.time = new Date().toString();
+        next();
+    },
+    (req, res, next) => {
+        res.send({
+            time: req.time,
+        });
+    }
+);
 
-app.use(urlEncodedMiddleware);
+/* 
+    body-parser middleware is used to parse the data coming from the
+    HTML form data, which is encoded in the query string format like
+    name=John+Doe&age=25
+
+    We can use it to decode the data into various formats. Here we will
+    use the bodyParser.json() to format the data in JSON.
+*/
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
 app.use(bodyParser.json());
 app.post('/name', (req, res) => {
+    /* 
+        The data from POST request can be acquired by using the 
+        req.body.key format
+    */
     return res.json({
-        name: `${req.body.first} ${req.body.last}`
-    })
-})
+        name: `${req.body.first} ${req.body.last}`,
+    });
+});
 
 app.get('/:word/echo', (req, res) => {
     return res.json({
-        echo: req.params.word
-    })
-})
+        echo: req.params.word,
+    });
+});
 
 app.get('/name', (req, res) => {
     return res.json({
-        name: `${req.query.first} ${req.query.last}`
-    })
-})
+        name: `${req.query.first} ${req.query.last}`,
+    });
+});
 
 let entryPath = `${__dirname}/views/index.html`;
 app.get('/', (request, response) => response.sendFile(entryPath));
